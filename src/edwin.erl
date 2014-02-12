e59d58e8e501c803abe8d75e030587dbbbdd76c3
Sergey Abramyan <sa.abramyan@gmail.com>
2014-02-12 15:18:16 +0400

    Add delete function

--- src/edwin.erl
+++ src/edwin.erl
@@ -5,6 +5,8 @@
 -export([select/1]).
 -export([update/2]).
 -export([insert/2]).
+-export([delete/1]).
+-export([delete/2]).
 -export([ex/1]).
 -export([ex/2]).
 
@@ -13,22 +15,31 @@ select(Table) ->
 select(Table, Columns) ->
     select(Table, Columns, []).
 select(Table, Columns, Where) ->
-    edwin_sql:select(Table, Columns, Where).
+    {SQL, Data} = edwin_sql:select(Table, Columns, Where),
+    emysql:execute(SQL, Data).
 
 update(Table, Args) ->
     update(Table, Args, []).
 update(Table, Args, Where) ->
-    edwin_sql:update(Table, Args, Where).
+    {SQL, Data} = edwin_sql:update(Table, Args, Where),
+    emysql:execute(SQL, Data).
 
 insert(Table, [{_,_} | _] = Query) when is_atom(Table)->
-    edwin_sql:insert(Table, Query).
+    {SQL, Data} = edwin_sql:insert(Table, Query),
+    emysql:execute(SQL, Data).
+
+delete(Table) ->
+    delete(Table, []).
+delete(Table, Where) ->
+    {SQL, Data} = edwin_sql:delete(Table, Where),
+    emysql:execute(SQL, Data).
 
 ex(SQL) ->
     ex(SQL, []).
-ex(SQL, Args) ->
+ex(SQL, Data) ->
     STM = random_atom(5),
     emysql:prepare(STM, SQL),
-    emysql:execute(pool0, STM, Args).
+    emysql:execute(pool0, STM, Data).
 
 random_atom(Len) ->
     Chrs = list_to_tuple("abcdefghijklmnopqrstuvwxyz"),
